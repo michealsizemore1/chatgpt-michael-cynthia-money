fd65ff3136b3290fb9b8203118e981e23ce43c99
michealsizemore1 <michealsizemore1@gmail.com>
2026-07-29 17:26:02 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mc-money-v3";
+const CACHE_NAME = "mc-money-v4";
 const CORE_ASSETS = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", event => {
